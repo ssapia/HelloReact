f4fc9715cc492ff02b3de6eeedd2c41f593858a8
salvador <salvador@ssapia.net>
2016-01-17 22:25:25 -0200

    Remove o que não esta sendo utilizado.

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -1,21 +1,10 @@
-/** In this file, we create a React component which incorporates components provided by material-ui */
 
 import React from 'react';
-import RaisedButton from 'material-ui/lib/raised-button';
-import Dialog from 'material-ui/lib/dialog';
 import ThemeManager from 'material-ui/lib/styles/theme-manager';
 import LightRawTheme from 'material-ui/lib/styles/raw-themes/light-raw-theme';
 import Colors from 'material-ui/lib/styles/colors';
-import FlatButton from 'material-ui/lib/flat-button';
-
-import TextField from 'material-ui/lib/text-field';
 
 const CommentBox = require('./CommentBox.react');
-
-
-const containerStyle = {
-  textAlign: 'center',
-};
 
 const Main = React.createClass({
 
@@ -44,42 +33,12 @@ const Main = React.createClass({
     this.setState({muiTheme: newMuiTheme});
   },
 
-  _handleRequestClose() {
-    this.setState({
-      open: false,
-    });
-  },
-
-  _handleTouchTap() {
-    this.setState({
-      open: true,
-    });
-  },
-
   render() {
-    const standardActions = (
-      <FlatButton
-        label="Okey"
-        secondary={true}
-        onTouchTap={this._handleRequestClose}
-      />
-    );
-
     return (
-	<CommentBox url="http://127.0.0.1:8081/api/comments" pollInterval={2000} />
+        <CommentBox url="http://127.0.0.1:8081/api/comments" pollInterval={2000} />
     );
   },
 });
 
-
-
-
-
-/*
-ReactDOM.render(
-  <CommentBox url="http://127.0.0.1:8081/api/comments" pollInterval={2000} />,
-  document.getElementById('content')
-);
-*/
 
 export default Main;
